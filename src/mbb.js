8e7fb858c40c552748784eefbf31c19db87eb33a
Kevin Lee <kevinmlee.x@gmail.com>
2015-09-09 15:48:03 -0700

    add wallpaper and form switching

--- src/mbb.js
+++ src/mbb.js
@@ -1,3 +1,16 @@
 $(document).ready(() => {
+  $('#mbb-picker-club').click((e) => {
+    $('#mbb-decal').fadeOut(300);
+    $('#mbb-club').delay(300).fadeIn(300);
 
+    $('#mbb-picker-club').addClass('active');
+    $('#mbb-picker-decal').removeClass('active');
+  });
+  $('#mbb-picker-decal').click((e) => {
+    $('#mbb-club').fadeOut(300);
+    $('#mbb-decal').delay(300).fadeIn(300);
+
+    $('#mbb-picker-decal').addClass('active');
+    $('#mbb-picker-club').removeClass('active');
+  });
 });
