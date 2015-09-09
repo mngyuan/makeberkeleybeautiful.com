4d9237577eff8fee9bab64dae3cf74695a6975b3
Kevin Lee <kevinmlee.x@gmail.com>
2015-09-09 16:20:50 -0700

    color pulsing logo

--- src/mbb.js
+++ src/mbb.js
@@ -13,4 +13,27 @@ $(document).ready(() => {
     $('#mbb-picker-decal').addClass('active');
     $('#mbb-picker-club').removeClass('active');
   });
+
+  $('#mbb-logo').addClass('loaded');
+
+  setInterval(_ => {
+    console.log($('body').attr('class'));
+    switch ($('body').attr('class')) {
+      case 'mbb-cyan':
+        $('body').attr('class', 'mbb-yellow');
+        break;
+      case 'mbb-yellow':
+        $('body').attr('class', 'mbb-magenta');
+        break;
+      case 'mbb-magenta':
+        $('body').attr('class', 'mbb-black');
+        break;
+      case 'mbb-black':
+        $('body').attr('class', 'mbb-cyan');
+        break;
+      default:
+        $('body').attr('class', 'mbb-cyan');
+        break;
+    }
+  }, 5000);
 });
