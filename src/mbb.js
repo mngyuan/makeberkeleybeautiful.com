7cc67a300e52090e2ade152ada4c68d842cfcf65
Kevin Lee <kevinmlee.x@gmail.com>
2015-09-09 18:24:56 -0700

    first pass ajax request for intro

--- src/mbb.js
+++ src/mbb.js
@@ -36,4 +36,35 @@ $(document).ready(() => {
         break;
     }
   }, 5000);
+
+  $('#mbb-decal').submit(_ => {
+    $.post(
+      url,
+      {
+        code: $('#decal-code').val(),
+        firstName: $('#decal-first_name').val(),
+        lastName: $('#decal-last_name').val(),
+        email: $('#decal-email').val(),
+        major: $('#decal-major').val(),
+        studentID: $('#decal-id').val(),
+        year: $('#decal-year').val(), // fixme
+        appliedBefore: $('#decal-before').val(),
+        appliedTimes: $('#decal-applied').val(),
+        units: $('#decal-units').val(),
+        usedIllustrator: $('#decal-illustrator').is(':checked'),
+        usedPhotoshop: $('#decal-photoshop').is(':checked'),
+        usedInDesign: $('#decal-indesign').is(':checked'),
+        usedGimp: $('#decal-gimp').is(':checked'),
+        usedInkscape: $('#decal-inkscape').is(':checked'),
+        usedOther: $('#decal-other').is(':checked'),
+        usedOtherText: $('#decal-other-text').val(),
+        experienceLevel: $('#decal-exp').val(), // fixme
+        why: $('#decal-why').val(),
+        real: $('#decal-real').val(),
+        link: $('#decal-link').val(),
+        mean: $('#decal-mean').val(),
+        sudden: $('#decal-sudden').val(),
+      }
+    );
+  });
 });
