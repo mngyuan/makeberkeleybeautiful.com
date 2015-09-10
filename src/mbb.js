f41a9c035f582f4a84c4512dea27a66719839c9e
Kevin Lee <kevinmlee.x@gmail.com>
2015-09-09 17:59:54 -0700

    add description to principles, default show intro, fix display bug

--- src/mbb.js
+++ src/mbb.js
@@ -1,17 +1,17 @@
 $(document).ready(() => {
   $('#mbb-picker-club').click((e) => {
     $('#mbb-decal').fadeOut(300);
-    $('#mbb-club').delay(300).fadeIn(300);
+    $('#mbb-pr').delay(300).fadeIn(300);
 
     $('#mbb-picker-club').addClass('active');
     $('#mbb-picker-decal').removeClass('active');
   });
   $('#mbb-picker-decal').click((e) => {
-    $('#mbb-club').fadeOut(300);
+    $('#mbb-pr').fadeOut(300);
     $('#mbb-decal').delay(300).fadeIn(300);
 
     $('#mbb-picker-decal').addClass('active');
-    $('#mbb-picker-club').removeClass('active');
+    $('#mbb-picker-pr').removeClass('active');
   });
 
   $('#mbb-logo').addClass('loaded');
